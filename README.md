5bc84d317b14e6a3c55b1c9964559761a9186dc9
Roman Inflianskas <infroma@gmail.com>
2018-10-07 12:05:02 +0300

    Add usage example in the README.md

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# ordered_set-stubs
-Stubs with type annotations for ordered-set Python library
+# ordered-set-stubs - stubs with type annotations for ordered-set Python library
+## Usage
+For example, you have the following code in `ordered_set_stubs_test.py` file:
+```python
+from ordered_set import OrderedSet
+
+
+# noinspection PyPep8Naming
+def receives_OrderedSet_int(ordered_set: 'OrderedSet[int]') -> 'OrderedSet[int]':
+    return ordered_set
+
+
+receives_OrderedSet_int(OrderedSet(['ololo']))
+```
+
+Run `mypy` to check the code and check that it returns an error:
+```
+$ mypy ordered_set_stubs_test.py
+ordered_set_stubs_test.py:10: error: List item 0 has incompatible type "str"; expected "int"
+```
+
+In Python 3.7 you can even drop quotes:
+```python
+from __future__ import annotations
+from ordered_set import OrderedSet
+
+
+# noinspection PyPep8Naming
+def receives_OrderedSet_int(ordered_set: OrderedSet[int]) -> OrderedSet[int]:
+    return ordered_set
+
+
+receives_OrderedSet_int(OrderedSet(['ololo']))
+```
--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'ordered-set-stubs',
-    'version': '0.1.0',
+    'version': '0.1.1',
     'description': 'Stubs with type annotations for ordered-set Python library',
-    'long_description': '# ordered_set-stubs\nStubs with type annotations for ordered-set Python library\n',
+    'long_description': '# ordered-set-stubs - stubs with type annotations for ordered-set Python library\n## Usage\nFor example, you have the following code in `ordered_set_stubs_test.py` file:\n```python\nfrom ordered_set import OrderedSet\n\n\n# noinspection PyPep8Naming\ndef receives_OrderedSet_int(ordered_set: \'OrderedSet[int]\') -> \'OrderedSet[int]\':\n    return ordered_set\n\n\nreceives_OrderedSet_int(OrderedSet([\'ololo\']))\n```\n\nRun `mypy` to check the code and check that it returns an error:\n```\n$ mypy ordered_set_stubs_test.py\nordered_set_stubs_test.py:10: error: List item 0 has incompatible type "str"; expected "int"\n```\n\nIn Python 3.7 you can even drop quotes:\n```python\nfrom __future__ import annotations\nfrom ordered_set import OrderedSet\n\n\n# noinspection PyPep8Naming\ndef receives_OrderedSet_int(ordered_set: OrderedSet[int]) -> OrderedSet[int]:\n    return ordered_set\n\n\nreceives_OrderedSet_int(OrderedSet([\'ololo\']))\n```\n',
     'author': 'Roman Inflianskas',
     'author_email': 'infroma@gmail.com',
     'url': 'https://github.com/rominf/ordered_set-stubs',
